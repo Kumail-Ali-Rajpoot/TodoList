e11d9636b55f735fbf89c87bfff915244ca2ffd5
Kumail Ali <m.kumailalirajpoot@gmail.com>
2025-05-29 12:02:09 +0100

    Frontend Complete

---
+++ app/edit/[id]/page.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+function page() {
+  return (
+  <div className="max-w-md mx-auto p-6 bg-white rounded-2xl shadow-md space-y-4">
+  <h2 className="text-2xl font-semibold text-center text-gray-800">Edit To TodoList</h2>
+
+  <div>
+    <label htmlFor="title" className="block text-sm font-medium text-gray-700">
+      Title
+    </label>
+    <input
+      type="text"
+      id="title"
+      name="title"
+      className="mt-1 block w-full rounded-xl border border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+      placeholder="Enter title"
+/>
+  </div>
+
+  <div>
+    <label htmlFor="description" className="block text-sm font-medium text-gray-700">
+      Description
+    </label>
+    <textarea
+      id="description"
+      name="description"
+      rows='10'
+      className="mt-1 block w-full rounded-xl border border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+      placeholder="Enter description"
+    />
+  </div>
+
+  <button
+    type="button"
+    className="w-full bg-blue-600 text-white py-2 px-4 rounded-xl hover:bg-blue-700 transition duration-200"
+  >
+    Submit
+  </button>
+</div>
+
+  )
+}
+
+export default page
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,26 @@
+'use client'
+
 import React from 'react'
 import { FaPlus } from 'react-icons/fa'
 import { MdDelete } from 'react-icons/md'
 import { FaPen } from 'react-icons/fa'
+import { useRouter } from 'next/navigation'
 
 function page() {
+  const {push} = useRouter();
+  function handleAdd () {
+    push("/add")
+  }
+  function handleUpdate () {
+    push("/edit/043045623446340");
+  }
   return (
     <>
     <div className='w-[100%] flex justify-center items-center'> {/* Parent container */ }
       <main className='mt-4 max-w-[1160px] w-[100%] border-2 border-black rounded-2xl p-2'> {/* Main container */ }
       <header className='w-[100%] flex justify-around items-center bg-black p-2'>
         <h1 className='text-3xl font-bold text-white'>TODO LIST</h1>
-        <button className='bg-blue-500 hover:cursor-pointer font-[500] rounded-[0.3rem] hover:bg-blue-400 text-white flex p-1 items-center w-[6rem] justify-around text-[1rem]'>
+        <button onClick={handleAdd} className='bg-blue-500 hover:cursor-pointer font-[500] rounded-[0.3rem] hover:bg-blue-400 text-white flex p-1 items-center w-[6rem] justify-around text-[1rem]'>
           Add <FaPlus/>
         </button>
       </header>
@@ -20,14 +30,14 @@ function page() {
             <p className='text-2xl '>Description</p>
           </div>
           <div className='flex text-[1rem] capitalize text-white absolute top-1 right-1 '>
-            <div className='hover:cursor-pointer hover:border-[1px] border-white p-1 flex font-bold gap-0.5'>
+            <button onClick={handleUpdate} className='hover:cursor-pointer capitalize hover:border-[1px] border-white p-1 flex font-bold gap-0.5'>
               edit
-            <MdDelete className='text-red-600 text-2xl'/>
-            </div>
-            <div className='hover:cursor-pointer hover:border-[1px] border-white p-1 flex font-bold gap-0.5'>
-              delete
             <FaPen className='text-black text-[1.25rem]'/>
-            </div>
+            </button>
+            <button className='hover:cursor-pointer hover:border-[1px] capitalize gap-1.5 border-white p-1 flex font-bold gap-0.5'>
+              delete
+            <MdDelete className='text-red-600 text-2xl'/>
+            </button>
           </div>
         </div>
       </main>
